1216c020a8594a21a4cd58232a89c8aa6582e26d
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-22 23:13:48 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,10 +6,7 @@ array.count do |element|
 end
 count_strings(array)
  end
- 
- end
    # Return the total number of strings in the provided array using the count enumerable
-end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
