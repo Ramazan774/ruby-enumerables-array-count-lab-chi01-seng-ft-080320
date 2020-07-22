5f7c53884cbcd5f283d16542e169559a902db66c
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-22 23:08:17 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,6 @@
 def count_strings(array)
  array.count do |count_strings|
-   count_strings = "String"
-   count_strings
+   count_strings(array)
  end
    # Return the total number of strings in the provided array using the count enumerable
 end
