1d857a5bb0fc6e1e47e035fba4b3ae594a5042e8
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-22 23:17:20 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,9 +1,5 @@
 def count_strings(array)
-array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
-array_2 = ["", 4, "", "goodbye",""]
-array = array_1.concat([array_2])
 count_strings(array)
-
  end
    # Return the total number of strings in the provided array using the count enumerable
 
